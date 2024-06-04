1690f37ae76959384966006cc2d5c6deb014f9fd
Rohit Sahu <89459208+rohitsahu70@users.noreply.github.com>
2024-06-04 10:16:31 +0530

    Update urls.py

--- data_management/urls.py
+++ data_management/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('file_upload/', file_upload, name='file-upload'),
     path('file_delete/<int:file_id>/', file_delete, name='file-delete'),
     path('file_manage/', file_manage, name='file-manage'),
-    path('analyze/<int:file_id>/', file_analyze, name='file-analyze'),
+    path('file_download/<int:file_id>/', file_download, name='file_download'),
     path('azure_blob_folders/', azure_blob_folders, name='azure_blob_folders'),
 ]
