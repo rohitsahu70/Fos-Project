bd337d86472a45e3f9c3e980bf61c74463475921
rohitsahu70 <rohitsahu70498@gmail.com>
2024-06-17 13:56:19 +0530

    change in file manage and views file

--- data_management/views.py
+++ data_management/views.py
@@ -12,9 +12,7 @@ from .models import UploadedFile
 from django.conf import settings
 from azure.storage.blob import BlobServiceClient
 from django.http import JsonResponse
-from fos_database_server.settings.development import AZURE_STORAGE_CONNECTION_STRING,AZURE_STORAGE_CONTAINER_NAME
-connection_string = AZURE_STORAGE_CONNECTION_STRING
-container_name = AZURE_STORAGE_CONTAINER_NAME
+from fos_database_server.settings.production import AZURE_STORAGE_CONNECTION_STRING,AZURE_STORAGE_CONTAINER_NAME
 
 @login_required
 def file_manage(request):
@@ -103,6 +101,8 @@ def file_upload(request):
     return render(request, 'upload.html', {'form': form})
 
 
+
+
 @login_required
 def file_delete(request, file_id):
     settings_file_name = settings.SETTINGS_MODULE.split('.')[-1]
@@ -119,6 +119,8 @@ def file_delete(request, file_id):
             fos_serial_number = file_to_delete.fos_serial_number
             file_data = str(file_to_delete.file)
             file_name = file_data.split('/')[-1]
+            connection_string=AZURE_STORAGE_CONNECTION_STRING
+            container_name=AZURE_STORAGE_CONTAINER_NAME
             blob_service_client = BlobServiceClient.from_connection_string(connection_string)
             blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{fos_serial_number}/{user_id}/{file_name}")
             blob_client.delete_blob()
@@ -127,8 +129,7 @@ def file_delete(request, file_id):
         else:
             messages.error(request, "Invalid request")
             return redirect('file-manage')
-
-
+        
 @login_required
 def file_download(request, file_id):
     if request.method == 'POST':
@@ -137,6 +138,8 @@ def file_download(request, file_id):
         fos_serial_number = file_to_download.fos_serial_number
         file_data = str(file_to_download.file)
         file_name = file_data.split('/')[-1]
+        connection_string=AZURE_STORAGE_CONNECTION_STRING
+        container_name=AZURE_STORAGE_CONTAINER_NAME
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{fos_serial_number}/{user_id}/{file_name}")
         try:
@@ -150,4 +153,4 @@ def file_download(request, file_id):
 
     else:
         messages.error(request, "Invalid request")
-        return redirect('file-manage')        
+        return redirect('file-manage')         
