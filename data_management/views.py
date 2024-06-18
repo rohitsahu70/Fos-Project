a204fa0eef1bbef4898a35f22b058b8e0830b1d1
rohitsahu70 <rohitsahu70498@gmail.com>
2024-06-18 16:25:41 +0530

    file_manage and views.py

--- data_management/views.py
+++ data_management/views.py
@@ -12,7 +12,9 @@ from .models import UploadedFile
 from django.conf import settings
 from azure.storage.blob import BlobServiceClient
 from django.http import JsonResponse
-from fos_database_server.settings.production import AZURE_STORAGE_CONNECTION_STRING,AZURE_STORAGE_CONTAINER_NAME
+from azure.core.exceptions import ResourceNotFoundError
+# from fos_database_server.settings.production import AZURE_STORAGE_CONNECTION_STRING,AZURE_STORAGE_CONTAINER_NAME
+from fos_database_server.settings.development import AZURE_STORAGE_CONNECTION_STRING,AZURE_STORAGE_CONTAINER_NAME
 
 @login_required
 def file_manage(request):
@@ -100,20 +102,17 @@ def file_upload(request):
                 messages.error(request, 'Please correct the errors below.')
     return render(request, 'upload.html', {'form': form})
 
-
-
-
 @login_required
 def file_delete(request, file_id):
     settings_file_name = settings.SETTINGS_MODULE.split('.')[-1]
     if request.method == 'POST':
-        if settings_file_name == "development":
+        if settings_file_name == "production":
             file_to_delete = get_object_or_404(FileUpload, pk=file_id)
             file_to_delete.file.delete() 
             file_to_delete.delete()
             return redirect('file-manage')
         
-        elif settings_file_name == "production":
+        elif settings_file_name == "development":
             file_to_delete = get_object_or_404(FileUpload, pk=file_id)
             user_id = request.user.id
             fos_serial_number = file_to_delete.fos_serial_number
@@ -123,12 +122,20 @@ def file_delete(request, file_id):
             container_name=AZURE_STORAGE_CONTAINER_NAME
             blob_service_client = BlobServiceClient.from_connection_string(connection_string)
             blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{fos_serial_number}/{user_id}/{file_name}")
-            blob_client.delete_blob()
+            
+            try:
+                blob_client.delete_blob()
+                messages.success(request, "File deleted successfully.")
+            except ResourceNotFoundError:
+                messages.error(request, f"The blob {fos_serial_number}/{user_id}/{file_name} does not exist.")
+                print(f"The blob {fos_serial_number}/{user_id}/{file_name} does not exist.")
+            
             file_to_delete.delete()
             return redirect('file-manage')
         else:
             messages.error(request, "Invalid request")
             return redirect('file-manage')
+
         
 @login_required
 def file_download(request, file_id):
